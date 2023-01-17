80e1feba9ed2049d167bee5227b3bc1b78e9d3b9
Louis Jeanneau <louis.bs.jeanneau@gmail.com>
2023-01-17 01:43:28 +0100

    Added COLORS and fixed everything ? Final commit

--- src/Main.java
+++ src/Main.java
@@ -9,18 +9,18 @@ public class Main {
 
     public static void main(String[] args) {
         // Welcome message
-        System.out.println("Welcome to SimpleDB ! A simple file-based database application developed in Java.");
+        System.out.println(ConsoleColors.WHITE_BOLD + "Welcome to SimpleDB ! A simple file-based database application developed in Java." + ConsoleColors.RESET);
 
         // Valid key inputs selection and user selection menu #1
         char[] correctKeys = {100, 68, 32};
         char pressedKey = 0;
         String line;
         while (!ArrayUtils.contains(correctKeys, pressedKey)) {
-            System.out.println("""
+            System.out.println(ConsoleColors.BLACK_BACKGROUND + """
                     Type an option (and then 'Enter' key) :
                     \t Spacebar : start command-line interface
                     \t D : Demo mode
-                    """);
+                    """ + ConsoleColors.RESET);
             line = scanner.nextLine();
             if (!Objects.equals(line, "")) {
                 pressedKey = line.charAt(0);
@@ -52,7 +52,6 @@ public class Main {
     private static void demoQueries() throws Exception {
         System.out.println("Launching the demo !\n");
         SimpleDB db = new SimpleDB("");
-        // TODO more demo
         String demo = """
                 help
                 SELECT * FROM notable
@@ -81,26 +80,28 @@ public class Main {
     }
 
     private static void cli() throws Exception {
-        System.out.println("Command-line Interface mode. Type 'exit' to exit.");
+        System.out.println(ConsoleColors.BLUE_BOLD + "Command-line Interface mode. Type 'exit' to exit. Type 'help' for help." + ConsoleColors.RESET);
 
         SimpleDB db = new SimpleDB("");
 
         String enteredLine;
         while (true){
             switch (enteredLine = scanner.nextLine()) {
-                case "" -> System.out.println("Empty query");
-                case "help" -> System.out.println("""
+                case "" -> System.out.println(ConsoleColors.CYAN + "Empty query" + ConsoleColors.RESET);
+                case "help" -> System.out.println(ConsoleColors.BLACK_BACKGROUND + """
                         You have access to the following commands :
                         \tCREATE TABLE name (column1, column2, ...)
                         \tINSERT INTO name VALUES (value1, value2, ...),  ...
                         \tUPDATE name SET column = 'newValue' WHERE condition
                         \tDELETE FROM name WHERE condition
-                        \tSELECT * FROM name WHERE condition GROUP BY column1
+                        \t\tWithout WHERE condition, deletes the whole table
+                        \tSELECT * FROM name WHERE condition GROUP BY column1, ...
                         \t\tWHERE clause, GROUP BY clause are optionals
                         \t\tYou can specify columns desired by replacing '*'
-                        """);
+                        \t\tYou can do CROSS JOIN by specifying multiples table after the FORM
+                        """ + ConsoleColors.RESET);
                 case "exit" -> {
-                    System.out.println("Exiting...");
+                    System.out.println(ConsoleColors.RED_BOLD + "Exiting..." + ConsoleColors.RESET);
                     return;
                 }
                 default -> db.executeSQL(enteredLine);
@@ -108,7 +109,7 @@ public class Main {
         }
     }
 
-    public static boolean userConfirmation(){
+    public static boolean userCancellation(){
         System.out.println("Type 'y' to confirm. Anything else to cancel.");
         char pressedKey = scanner.nextLine().charAt(0);
         return pressedKey != 'y';
--- src/SimpleDB.java
+++ src/SimpleDB.java
@@ -6,15 +6,16 @@ import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class SimpleDB {
+    //region REGEX
     private static final String CREATE_TABLE_REGEX = "CREATE TABLE (\\w+) \\(([\\w, ]+)\\)";
     private static final String INSERT_REGEX = "INSERT INTO (\\w+) VALUES ((\\(([\\w ,']+)\\),* *)+)";
     private static final String UPDATE_REGEX = "UPDATE (\\w+) SET (((\\w+) ?= ?'([\\w ]+)' *,* *)+).*";
     private static final String DELETE_REGEX = "DELETE FROM (\\w+)(.*)";
-    private static final String SELECT_REGEX = "SELECT ([\\w, ]+|\\*) FROM ((?:\\w+[^WHER|GOUP BY],* *)+)(.*)";
-    private static final String WHERE_REGEX = "WHERE (((\\w+) *= *'(\\w+)' *(AND)* *)+).*";
-    private static final String GROUP_REGEX = "GROUP BY (((\\w+) *,* *)+)";
-    private static final String JOIN_REGEX = "JOIN ([\\w, ]+|\\*).*";
+    private static final String SELECT_REGEX = "SELECT ([\\w, ]+|\\*) FROM (.+) ?(?:(WHERE)|(GROUP BY))?";
+    private static final String WHERE_REGEX = "WHERE ((?:\\w+ ?= ?'\\w+' *(?:AND)* *)+)";
+    private static final String GROUP_REGEX = "GROUP BY ((?:\\w+ *,* *)+)";
     private static final String TRIM_REGEX = "^[( '\"]+|[) '\"]+$";
+    //endregion
 
     Map<String, Table> tables;
 
@@ -24,11 +25,12 @@ public class SimpleDB {
         loadFromFile(folderName);
     }
 
-    /**
+    /** execute a SQL query
      * @param sql the SQL query to execute
      */
     public void executeSQL(String sql) {
-        System.out.println("You typed : " + sql);
+        System.out.println("You typed : " + ConsoleColors.GREEN + sql + ConsoleColors.RESET);
+        sql = sql.trim();
         try {
             // Create table
             if (sql.matches(CREATE_TABLE_REGEX)) {
@@ -50,19 +52,22 @@ public class SimpleDB {
             else if (sql.matches(SELECT_REGEX)) {
                 handleSelect(sql);
             } else {
-                System.out.println("Statement not recognized");
+                System.out.println(ConsoleColors.RED +  "Statement not recognized" + ConsoleColors.RESET);
             }
         } catch (CancellationException e) {
             System.out.println(e.getMessage());
         } catch (IllegalArgumentException e) {
-            System.out.println("Your prompt is invalid : " + e.getMessage());
-        } catch (NullPointerException e){
+            System.out.println(ConsoleColors.RED_BOLD +  "Your prompt is invalid : " + ConsoleColors.RED + e.getMessage() + ConsoleColors.RESET);
+        } catch (NullPointerException e) {
             System.out.println("This table does not exist");
         }
+        System.out.println();
     }
 
+    //region TRIVIAL HANDLES
     /**
      * Table creation, with overwrite check and confirmation
+     *
      * @param sql valid CREATE sql query
      */
     private void handleCreate(String sql) {
@@ -75,10 +80,10 @@ public class SimpleDB {
         // Trim leading/trailing whitespace from column names
         columnNames = Arrays.stream(columnNames).map(String::trim).toArray(String[]::new);
 
-        // Override check and validation by user
+        // Overwrite check and validation by user
         if (tables.containsKey(tableName)) {
             System.out.println("This table already exists. This command will overwrite the existing table. Do you agree ?");
-            if (Main.userConfirmation()) {
+            if (Main.userCancellation()) {
                 throw new CancellationException("Creation of table canceled by user");
             }
         }
@@ -103,22 +108,166 @@ public class SimpleDB {
         m.find();
         String tableName = m.group(1);
         String[] values = m.group(2).split("\\), *\\(");
-        String[] row;
+        String[] singleValue;
 
-        for (String value : values) {
-            // Trim leading/trailing whitespace from values
-            row = Arrays.stream(value.split(",")).map(c -> c.replaceAll(TRIM_REGEX, "")).toArray(String[]::new);
+        int i = 0;
+        // for each tuple of value :
+        for (String v : values) {
+            // Trim leading/trailing whitespace from the tuple of value
+            singleValue = Arrays.stream(v.split(",")).map(c -> c.replaceAll(TRIM_REGEX, "")).toArray(String[]::new);
 
             // Insert row into table
-            tables.get(tableName).insert(row);
+            if (tables.get(tableName).insert(singleValue))
+                i += 1;
         }
-
+        // Information
+        System.out.println(i + " row(s) inserted");
 
         // Saving to files
         onExecutionSaving(tableName);
     }
 
+    /**
+     * Update the table
+     *
+     * @param sql a valid UPDATE query
+     */
+    private void handleUpdate(String sql) {
+        // Parse table name, column name, value, and WHERE clause from SQL
+        Matcher m = Pattern.compile(UPDATE_REGEX).matcher(sql);
+        m.find();
+        String tableName = m.group(1);
+        String[] updates = m.group(2).split(",");
 
+        // Put all the column name in an array, and new values in another
+        String[] updateColumns = Arrays.stream(updates).map(c -> c.split("=")[0].trim()).toArray(String[]::new);
+        String[] updateValues = Arrays.stream(updates).map(c -> c.split("=")[1].trim().replaceAll(TRIM_REGEX, "")).toArray(String[]::new);
+
+        // Get the table
+        Table table = tables.get(tableName);
+
+        // Handle the WHERE clause
+        List<String[]> rows = handleWhere(sql, table);
+
+        // Update rows in table
+        if (table.update(rows, updateColumns, updateValues)) {
+            System.out.println(rows.size() + " row(s) updated");
+        } else {
+            System.out.println("0 row updated");
+        }
+
+        onExecutionSaving(tableName);
+    }
+
+    /**
+     * Delete element matching the WHERE condition. If no WHERE condition, delete everything
+     *
+     * @param sql valid DELETE FROM query
+     */
+    private void handleDelete(String sql) {
+        // Parse table name and WHERE clause from SQL
+        Matcher m = Pattern.compile(DELETE_REGEX).matcher(sql);
+        m.find();
+        String tableName = m.group(1);
+        String whereSQL = m.group(2);
+
+        // Get table
+        Table table = tables.get(tableName);
+
+        List<String[]> selectedRows;
+        // Delete everything ?
+        if (Pattern.compile(WHERE_REGEX).matcher(whereSQL).find()) {
+            // Select the line according to conditions
+            selectedRows = handleWhere(whereSQL, table);
+        }
+        else {
+            // Ask for confirmation
+            System.out.println(ConsoleColors.RED + "You are about to delete the whole " + tableName + " table." + ConsoleColors.RESET);
+            if (Main.userCancellation()) {
+                throw new CancellationException("Canceled deletion of the whole table.");
+            }
+            selectedRows = table.getRows();
+
+        }
+
+
+        // Delete rows from table
+        if (table.deleteRows(selectedRows)) {
+            System.out.println(selectedRows.size() + " row(s) deleted");
+        } else {
+            System.out.println("0 row deleted");
+        }
+
+        // Saving
+        onExecutionSaving(tableName);
+    }
+
+    /**
+     * Print the result of your query
+     *
+     * @param sql a SELECT query
+     */
+    private void handleSelect(String sql) {
+        // Extract selected columns from the sql
+        Matcher m = Pattern.compile(SELECT_REGEX).matcher(sql);
+        m.find();
+        String columnsString = m.group(1);
+        String otherPart = m.group(2);
+
+        // Okay, so. This is a mess. I find the name of the table(s) by looking at the shortest string when I cut of the GROUP and WHERE regex
+        String[] potentialTableName = {otherPart, Pattern.compile(WHERE_REGEX).split(otherPart)[0],  Pattern.compile(GROUP_REGEX).split(otherPart)[0]};
+        String tableName = Arrays.stream(potentialTableName).min(Comparator.comparingInt(String::length)).get();
+
+        // Get table, CROSS JOIN aware
+        Table table;
+        if (tableName.split(",").length > 1) {
+            // CROSS JOIN
+            table = handleCrossJoin(tableName);
+        } else {
+            table = tables.get(tableName.replaceAll(TRIM_REGEX, ""));
+        }
+
+        // Select rows if WHERE condition
+        List<String[]> rows;
+        if (Pattern.compile(WHERE_REGEX).matcher(otherPart).find()) {
+            rows = handleWhere(otherPart, table);
+        } else {
+            rows = table.getRows();
+        }
+
+        // GROUP BY
+        if (Pattern.compile(GROUP_REGEX).matcher(otherPart).find()) {
+            rows = handleGroupBy(otherPart, rows, table);
+        }
+
+        // Extract columns indexes if not *
+        int[] columnsIndex;
+        if (columnsString.equals("*")) {
+            columnsIndex = IntStream.range(0, table.getColumns().length).toArray();
+        } else {
+            String[] columns = Arrays.stream(columnsString.split(",")).map(c -> c.replaceAll(TRIM_REGEX, "")).toArray(String[]::new);
+            columnsIndex = table.getColumnsIndex(columns);
+        }
+
+        // Print the table
+        String[] col = table.getColumns();
+        System.out.printf(ConsoleColors.WHITE_BACKGROUND + ConsoleColors.BLUE);
+        for (int index : columnsIndex) {
+            System.out.printf(col[index] + ", ");
+        }
+        System.out.print(ConsoleColors.RESET + "\b\b  \n" + ConsoleColors.BLACK_BACKGROUND);
+        // Rows
+        for (String[] row : rows) {
+            for (int index : columnsIndex) {
+                System.out.printf(row[index] + ", ");
+            }
+            System.out.print("\b\b  \n");
+        }
+        System.out.printf(ConsoleColors.RESET);
+    }
+    //endregion
+
+    //region FILTERING handles
     /**
      * extract the lines according to the query from the table<
      *
@@ -132,6 +281,11 @@ public class SimpleDB {
         List<String[]> conditions = new ArrayList<>();
         for (String condition : conditionsString) {
             conditions.add(Arrays.stream(condition.split("=")).map(c -> c.replaceAll(TRIM_REGEX, "")).toArray(String[]::new));
+        }
+
+        // Check if we found conditions
+        if (conditions.get(0)[0].equals("")){
+            throw new IllegalArgumentException("There is no WHERE condition while it's required, or your condition is badly written");
         }
 
         // Extract columns indexes
@@ -157,138 +311,13 @@ public class SimpleDB {
         return filteredRows;
     }
 
-    /**
-     * Update the table
-     * @param sql a valid UPDATE query
-     */
-    private void handleUpdate(String sql) {
-        // Parse table name, column name, value, and WHERE clause from SQL
-        Matcher m = Pattern.compile(UPDATE_REGEX).matcher(sql);
-        m.find();
-        String tableName = m.group(1);
-        String[] updates = m.group(2).split(",");
-        String[] updateColumns = Arrays.stream(updates).map(c -> c.split("=")[0].trim()).toArray(String[]::new);
-        String[] updateValues = Arrays.stream(updates).map(c -> c.split("=")[1].trim().replace("'", "")).toArray(String[]::new);
-
-        Table table = tables.get(tableName);
-        List<String[]> rows = handleWhere(sql, table);
-        // Update rows in table
-         if (table.update(rows, updateColumns, updateValues)){
-             System.out.println(rows.size() + " row(s) updated");
-         }
-         else {
-             System.out.println("0 row updated");
-         }
-
-        onExecutionSaving(tableName);
-    }
-
-    /**
-     * Delete element matching the WHERE condition. If no WHERE condition, delete everything
-     * @param sql valid DELETE FROM query
-     */
-    private void handleDelete(String sql) {
-        // Parse table name and WHERE clause from SQL
-        Matcher m = Pattern.compile(DELETE_REGEX).matcher(sql);
-        m.find();
-        String tableName = m.group(1);
-        String whereSQL = m.group(2);
-
-        // Get table
-        Table table = tables.get(tableName);
-
-        List<String[]> selectedRows;
-        // Delete everything ?
-        if (whereSQL.equals("")){
-            System.out.println("You are about to delete the whole " + tableName + " table.");
-            if (Main.userConfirmation()) {
-                throw new CancellationException("Canceled deletion of the whole table.");
-            }
-            selectedRows = table.getRows();
-
-        } // Select the line according to conditions
-        else {
-            selectedRows = handleWhere(whereSQL, table);
-        }
-
-        // Delete rows from table
-        if (table.deleteRows(selectedRows)){
-            System.out.println(selectedRows.size() + " row(s) deleted");
-        }
-        else {
-            System.out.println("0 row deleted");
-        }
-
-        // Saving
-        onExecutionSaving(tableName);
-    }
-
-    /**
-     * Print the result of your query
-     * @param sql a SELECT query
-     */
-    private void handleSelect(String sql) {
-        // Parse table name and WHERE clause from SQL
-        Matcher m = Pattern.compile(SELECT_REGEX).matcher(sql);
-        m.find();
-        String tableName = m.group(2);
-        String columnsString = m.group(1);
-        String whereSQL = m.group(3);
-
-        // Get table
-        Table table;
-        if (tableName.split(",").length > 1){
-            // CROSS JOIN
-            table = handleCrossJoin(tableName);
-        } else {
-            table = tables.get(tableName.replaceAll(TRIM_REGEX, ""));
-        }
-
-        // Select rows if WHERE condition
-        List<String[]> rows;
-        m = Pattern.compile(WHERE_REGEX).matcher(sql);
-        if (m.find()) {
-            rows = handleWhere(whereSQL, table);
-        }
-        else {
-            rows = table.getRows();
-        }
-
-        // GROUP BY
-        m = Pattern.compile(GROUP_REGEX).matcher(sql);
-        if (m.find()){
-            rows = handleGroupBy(m.group(1), rows, table);
-        }
-
-        // Extract columns indexes if not *
-        int[] columnsIndex;
-        if (columnsString.equals("*")){
-            columnsIndex = IntStream.range(0, table.getColumns().length).toArray();
-        }
-        else {
-            String[] columns = Arrays.stream(columnsString.split(",")).map(c -> c.replaceAll(TRIM_REGEX, "")).toArray(String[]::new);
-            columnsIndex = table.getColumnsIndex(columns);
-        }
-
-        // Print the table
-        // Columns
-        String[] col = table.getColumns();
-        for (int index : columnsIndex) {
-            System.out.printf(col[index] + ", ");
-        }
-        System.out.print("\b\b  \n");
-        // Rows
-        for (String[] row : rows) {
-            for (int index : columnsIndex) {
-                System.out.printf(row[index] + ", ");
-            }
-            System.out.print("\b\b  \n");
-        }
-    }
-
-    private List<String[]> handleGroupBy(String columnsString, List<String[]> rows, Table table){
+    private List<String[]> handleGroupBy(String sql, List<String[]> rows, Table table) {
         List<String[]> resultRows = new ArrayList<>();
-               
+
+        Matcher m = Pattern.compile(GROUP_REGEX).matcher(sql);
+        m.find();
+        String columnsString = m.group(1);
+
         // Extract columns indexes
         String[] columns = Arrays.stream(columnsString.split(",")).map(c -> c.replaceAll(TRIM_REGEX, "")).toArray(String[]::new);
         int[] columnsIndex = table.getColumnsIndex(columns);
@@ -301,16 +330,16 @@ public class SimpleDB {
                 rowSelectedColumn.add(row[index]);
             }
             String[] rowSelectedColumnArray = rowSelectedColumn.toArray(String[]::new);
-            if (encountered.stream().noneMatch(c -> Table.equalsRow(c, rowSelectedColumnArray))){
+            if (encountered.stream().noneMatch(c -> Table.equalsRow(c, rowSelectedColumnArray))) {
                 resultRows.add(row.clone());
                 encountered.add(rowSelectedColumnArray);
             }
         }
-        
-        return  resultRows;
-    }
 
-    private Table handleCrossJoin(String namesString){
+        return resultRows;
+    }
+    //endregion
+    private Table handleCrossJoin(String namesString) {
         List<String> columns = new ArrayList<>();
         List<String> tablesName = Arrays.stream(namesString.split(",")).map(c -> c.replaceAll(TRIM_REGEX, "")).toList();
         for (String t : tablesName) {
@@ -339,6 +368,7 @@ public class SimpleDB {
         return joinedTable;
     }
 
+    //region FILE READ/WRITE
     private void loadFromFile(String folderName) throws Exception {
         // Get list of CSV files in directory
         File dir = new File("." + folderName + "\\");
@@ -354,9 +384,11 @@ public class SimpleDB {
 
     private void onExecutionSaving(String tableName) {
         try {
-            tables.get(tableName).saveToCSV(tableName + ".csv");
+            if (!tables.get(tableName).saveToCSV(tableName + ".csv"))
+                System.out.println("Something went wrong while saving table " + tableName);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
+    //endregion
 }
